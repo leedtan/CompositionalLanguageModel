Fix leakage

dataProcess.py
@@ -3,6 +3,8 @@
 import argparse
 import pandas as pd
 import os
+import numpy as np
+import pickle
 
 def readTaskFile(filename):
     df = pd.read_csv(filename, sep = ': ', names = ['_', 'Input', 'Output'], engine='python')
@@ -13,7 +15,9 @@ def readTaskFile(filename):
 def readLengthFile(filename):
     df = pd.read_csv(filename, sep = ' ::: ', names = ['Input', 'Output', 'Length'], engine='python')
     df['Input'] = df['Input'].map(lambda x: x[4:])
-    return df
+    uni_commands = set(' '.join(list(df['Input'])).split())
+    uni_actions = set(' '.join(list(df['Output'])).split())
+    return df, uni_commands, uni_actions
 
 def getLengthTag(dfTask, dfLength):
     dfTask2 = pd.merge(dfTask, dfLength[['Input', 'Length']], on = 'Input')
@@ -22,7 +26,22 @@ def getLengthTag(dfTask, dfLength):
     else:
         print('%d records not matched'%(len(dfTask) - len(dfTask2)))
 
+def build_fmap_invmap(unique, num_unique):
+    fmap = dict(zip(unique, np.arange(num_unique) )) # Use 0 for padding
+    invmap = dict(zip(np.arange(num_unique), unique))
+    return fmap, invmap
 
+def buildDict(uni_commands, uni_actions):
+    uni_commands.add('end_command')
+    uni_actions.add('end_subprogram')
+    uni_actions.add('start_subprogram')
+    uni_actions.add('end_action')
+    uni_actions.add('start_action')
+    num_cmd = len(uni_commands)
+    num_act = len(uni_actions)
+    command_map, command_invmap = build_fmap_invmap(uni_commands, num_cmd)
+    action_map, action_invmap = build_fmap_invmap(uni_actions, num_act)
+    return command_map, command_invmap, action_map, action_invmap
 
 
 if __name__ == '__main__':
@@ -32,9 +51,12 @@ if __name__ == '__main__':
     parser.add_argument('--outPath')  # output folder
 
     args = parser.parse_args()
-    dfLength = readLengthFile(args.lengthFileName)
+    dfLength, uni_commands, uni_actions = readLengthFile(args.lengthFileName)
+    command_map, command_invmap, action_map, action_invmap = buildDict(uni_commands, uni_actions)
+    pickle.dump([command_map, command_invmap, action_map, action_invmap], open(args.outPath + 'maps.p', 'wb'))
     n = len(args.filePath) # To be used to separate root path and subdirectory below
 
+    """
     for dirName, subdirList, fileList in os.walk(args.filePath):
         #print('entries: %s, %s, %s'%(dirName, subdirList, fileList))
         for file in fileList:
@@ -51,39 +73,9 @@ if __name__ == '__main__':
                     with open(outFile, 'w') as f:
                         for _, row in dfTask2.iterrows():
                             f.write("::: " + row['Input'] + " ::: " + row['Output'] + " ::: " + row['Length'] + '\n')
+    """
 
 
 
 
 
-
-
-
-"""
-EOS_token = 1
-SOS_token = 0
-
-def _build_vocab(filename):
-  data = _read_words(filename)
-
-  counter = collections.Counter(data)
-  count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
-
-  words, _ = list(zip(*count_pairs))
-  word_to_id = dict(zip(words, range(len(words))))
-
-  return word_to_id
-
-
-def _file_to_word_ids(filename, word_to_id):
-  data = _read_words(filename)
-  return [word_to_id[word] for word in data if word in word_to_id]
-
-df = readLengthFile('tasks_with_length_tags.txt')
-df2 = readTaskFile('tasks_test_simple.txt')
-
-df3 = getLengthTag(df2, df)
-
-df3 = pd.merge(df2, df[['Input', 'Length']], on = 'Input')
-
-"""

modelV1_tf.py
@@ -42,11 +42,13 @@ def file2np(commands, actions, structures, command_map, action_map, max_cmd_len,
             action_row.append([action_map['start_action']] + a + [action_map['end_action']] + [0] * padding)
             start = end
         actions_structured.append(
-            action_row + [[action_map['end_subprogram']] + [0] * (max_actions_per_subprogram - 1)] +
-            [[0] * max_actions_per_subprogram] * (max_num_subprograms - len(struct) - 1)
+            [[action_map['start_subprogram']] + [0] * (max_actions_per_subprogram - 1)] + action_row +
+            [[action_map['end_subprogram']] + [0] * (max_actions_per_subprogram - 1)] +
+            [[0] * max_actions_per_subprogram] * (max_num_subprograms - len(struct) - 2)
         )
     act_np = np.array(actions_structured)
-    struct_padded = [[sa + 1 for sa in s] + [1] + [0] * (max_num_subprograms - len(s) - 1) for s in structures] # Add end
+    struct_padded = [[1] + [sa + 1 for sa in s] + [1] + [0] * (max_num_subprograms - len(s) - 2) for s in structures]
+    # sa+1: Add end_act; start_act token directly used as first input to subprogram
     struct_np = np.array(struct_padded)
 
     mask_list = [[np.concatenate((np.ones(st), np.zeros(max_actions_per_subprogram - st)), 0) for st in s] for s in struct_np]
@@ -196,7 +198,7 @@ class m1():
         self.num_cmd = model_paras.get('num_cmd', 14)
         self.num_act = model_paras.get('num_act', 9)
         self.max_cmd_len = model_paras.get('max_cmd_len', 10)
-        self.max_num_subprograms = model_paras.get('max_num_subprograms', 7)
+        self.max_num_subprograms = model_paras.get('max_num_subprograms', 8)
         self.max_actions_per_subprogram = model_paras.get('max_actions_per_subprogram', 10)
 
         self.hidden_filters = model_paras.get('hidden_filters', 128)
@@ -218,6 +220,7 @@ class m1():
         self.cmd_lengths = tf.placeholder(tf.int32, shape=(None,))
         self.act_lengths = tf.placeholder(tf.int32, shape=(None, self.max_num_subprograms))
         self.learning_rate = tf.placeholder(tf.float32, shape=(None))
+        self.isTrain = tf.placeholder_with_default(True, shape = None)
 
         ## Embeddings
         self.cmd_mat = tf.Variable(self.init_mag * tf.random_normal([self.num_cmd, self.size_emb]))
@@ -263,10 +266,11 @@ class m1():
         initial_cmb_encoding = last_encoding
 
         action_probabilities_presoftmax = []
+        autoregressive = act_emb[:, 0, :, :]  # Initiate step
+
         # Iterate through each subprogram
         for sub_idx in range(self.max_num_subprograms):
             from_last_layer = tf.tile(tf.expand_dims(tf.concat((initial_cmb_encoding, last_encoding), 1), 1), [1, self.max_actions_per_subprogram, 1])  # Shape bs x (self.max_actions_per_subprogram) x LSTM_outx2
-            autoregressive = act_emb[:, sub_idx, :, :]
             x_input = tf.concat((from_last_layer, autoregressive), -1)  # bs x (self.max_actions_per_subprogram) x (input dim)
             subprogram_last_layer, _, subprogram_hidden_layers, subprogram_output = subprogram(x_input, self.num_layers_subprogram, cells_subprogram, None,
                 lengths=self.act_lengths[:, sub_idx], reuse=(sub_idx > 0), name='subprogram')  # Reuse the same network across subprogram
@@ -280,6 +284,10 @@ class m1():
             insert = tf.tanh(delta2) + delta2 / 100
             last_encoding = last_encoding * remember + insert
             encodings.append(last_encoding)
+            # Use prediction in test run
+            action_taken_layer = tf.argmax(action_probabilities_layer, -1, output_type=tf.int32)
+            act_emb_pred_layer = tf.nn.embedding_lookup(self.act_mat, action_taken_layer)
+            autoregressive = tf.cond(self.isTrain, lambda: act_emb[:, sub_idx, :, :], lambda: act_emb_pred_layer)
 
         ## Loss
         #act_presoftmax = tf.stack(action_probabilities_presoftmax, 1)[:, :, 1:-1, :]
@@ -308,6 +316,8 @@ class m1():
         opt_fcn = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
         # opt_fcn = tf.train.MomentumOptimizer(learning_rate=learning_rate, use_nesterov=True, momentum=.8)
         self.optimizer, grad_norm_total = util.apply_clipped_optimizer(opt_fcn, self.loss)
+
+
 
 
 #=========== Training module =============
@@ -365,7 +375,8 @@ class trainModel:
 
     def _trainEpoch(self, sess, iter, trn_loss_avg, acc_trn_avg, acc_trn_single_avg):
         cmd, act, mask, struct, cmd_length, idx = self.trainset.next_batch(self.batchSize, isTrain=True)
-        trn_feed_dict = {self.m.cmd_ind: cmd, self.m.act_ind: act, self.m.mask_ph: mask, self.m.act_lengths: np.clip(struct, a_min=1, a_max=None), self.m.cmd_lengths: cmd_length}
+        trn_feed_dict = {self.m.cmd_ind: cmd, self.m.act_ind: act, self.m.mask_ph: mask, self.m.isTrain: True,
+                         self.m.act_lengths: np.clip(struct, a_min=1, a_max=None), self.m.cmd_lengths: cmd_length}
         trn_feed_dict[self.m.learning_rate] = .02 / (np.power(iter + 10, .6))
         _, trn_loss, acc_trn_single, acc_trn = sess.run([self.m.optimizer, self.m.loss, self.m.percent_correct, self.m.percent_fully_correct], trn_feed_dict)
 
@@ -384,7 +395,8 @@ class trainModel:
         pred = []
         for i in range(self.nBatchTest):
             cmd, act, mask, struct, cmd_length, idx = self.testset.next_batch(self.batchSize, isTrain=False)
-            val_feed_dict = {self.m.cmd_ind: cmd, self.m.act_ind: act, self.m.mask_ph: mask, self.m.act_lengths: np.clip(struct, a_min=1, a_max=None), self.m.cmd_lengths: cmd_length,}
+            val_feed_dict = {self.m.cmd_ind: cmd, self.m.act_ind: act, self.m.mask_ph: mask, self.m.isTrain: False,
+                             self.m.act_lengths: np.clip(struct, a_min=1, a_max=None), self.m.cmd_lengths: cmd_length,}
             val_loss, acc_val, pred_i = sess.run([self.m.loss, self.m.percent_fully_correct, self.m.logprobabilities], val_feed_dict)
             bs = len(cmd)
             val_loss_all += val_loss